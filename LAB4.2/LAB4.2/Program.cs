757a1538bf898e8d50f76d396e75132b5a209e40
Vitos <rustamminis@gmail.com>
2025-11-24 14:22:53 +0200

    Add files via upload

---
+++ LAB4.2/LAB4.2/Program.cs
@@ -0,0 +1,36 @@
+﻿try
+{
+int x = 0;
+Random rand = new Random();
+int[,] mas = new int[7, 8];
+Console.WriteLine("Исходный массив: ");
+for (int i = 0; i < mas.GetLength(0); i++)
+{
+    for (int j = 0; j < mas.GetLength(1); j++)
+    {
+        mas[i, j] = rand.Next(1, 10);
+        Console.Write(mas[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+    int[] mas1 = new int[8];
+    for (int j = 0; j < mas.GetLength(1); j++)
+{
+    x = 0;    
+    for (int i = 0; i < mas.GetLength(0); i++)
+    {
+        if (mas[i, j] % 2 != 0)
+        {
+            x++;
+        }
+    }
+    mas1[j] = x;
+    Console.Write($"(Столбец:{j+1} Количество нечетных элементов:{mas1[j]}) ");
+}
+Console.WriteLine();
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+
+}
